89ff2fcbc62d3b0649018d175a034b15cbe30da1
Pranav Jain <pranavjn.kota@gmail.com>
2025-01-24 22:31:13 +0530

    HomePage style

--- ui/src/components/HomePage/index.tsx
+++ ui/src/components/HomePage/index.tsx
@@ -3,6 +3,7 @@ import { GoogleAuthProvider, signInWithPopup } from "firebase/auth";
 import { auth } from "../../firebase";
 import { useNavigate } from "react-router-dom";
 import { AuthContext } from "../../context/AuthContext";
+import { ReactComponent as GoogleIcon } from "../../assets/icons/google-icon.svg";
 
 const HomePage = () => {
   const navigate = useNavigate();
@@ -26,18 +27,17 @@ const HomePage = () => {
   };
 
   return (
-    <div style={{ textAlign: "center", marginTop: "50px" }}>
-      <h1>Login with Google</h1>
-      <button
-        onClick={handleGoogleLogin}
-        style={{
-          padding: "10px 20px",
-          fontSize: "16px",
-          cursor: "pointer",
-        }}
-      >
-        Login with Google
-      </button>
+    <div className="vh-100 vw-100 d-grid justify-content-center align-items-center">
+      <div className="d-flex flex-column gap-3 align-items-center">
+        <div>Welcome to my Chatbot app!</div>
+        <div>Please login</div>
+        <button
+          onClick={handleGoogleLogin}
+          className="btn btn-outline-primary d-grid justify-content-center align-items-center p-3"
+        >
+          <GoogleIcon height={"20px"} width={"20px"} />
+        </button>
+      </div>
     </div>
   );
 };
