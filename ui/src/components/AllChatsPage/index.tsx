45ab16b0a9f15ca8ea1b0f4d77dd55e592de6356
Pranav Jain <pranavjn.kota@gmail.com>
2025-01-25 06:44:04 +0530

    chat session deletion

--- ui/src/components/AllChatsPage/index.tsx
+++ ui/src/components/AllChatsPage/index.tsx
@@ -1,33 +1,53 @@
 import React from "react";
+import "./AllChatsPage.scss";
 import useAllChats from "./useAllChats";
 import { Link } from "react-router-dom";
+import { ReactComponent as DeleteIcon } from "../../assets/icons/close-icon.svg";
+import LoadingSpinner from "../LoadingSpinner";
+import { cx } from "@emotion/css";
 
 const AllChatsPage: React.FC = () => {
-  const { chatSessions } = useAllChats();
+  const { chatSessions, deleteChat, isApiCallOngoing, loading } = useAllChats();
 
   return (
     <div className="container py-3">
       <Link to={"/chat/new"}>
         <button className="btn btn-primary mb-3">+ New Chat</button>
       </Link>
-      <div className="row">
-        {chatSessions.map((session) => (
-          <div className="col-12 col-md-4 mb-3" key={session.id}>
-            <a
-              href={`/chat/${session.id}`}
-              className="card"
-              style={{ textDecoration: "none", color: "inherit" }}
+      {loading ? (
+        <LoadingSpinner />
+      ) : (
+        <div className="row">
+          {chatSessions.map((session) => (
+            <div
+              className="chat-session col-12 col-md-4 mb-3 position-relative"
+              key={session.id}
             >
-              <div className="card-body">
-                <h5 className="card-title">{session.name}</h5>
-                <p className="card-text">
-                  {new Date(session.created).toDateString()}
-                </p>
-              </div>
-            </a>
-          </div>
-        ))}
-      </div>
+              <Link
+                to={`/chat/${session.id}`}
+                className={cx("card chat-session-card", {
+                  "disabled-link": isApiCallOngoing,
+                })}
+                style={{ textDecoration: "none", color: "inherit" }}
+              >
+                <div className="card-body">
+                  <h5 className="card-title">{session.name}</h5>
+                  <p className="card-text">
+                    {new Date(session.created).toDateString()}
+                  </p>
+                </div>
+              </Link>
+              <button
+                onClick={() => deleteChat(session.id)}
+                className="btn btn-light delete-button p-1"
+                disabled={isApiCallOngoing}
+              >
+                <DeleteIcon height="20px" className="delete-icon" />
+              </button>
+            </div>
+          ))}
+        </div>
+      )}
     </div>
   );
 };
--- ui/src/components/AllChatsPage/useAllChats.ts
+++ ui/src/components/AllChatsPage/useAllChats.ts
@@ -4,21 +4,40 @@ import { axiosAuthInstance } from "../../utils/api";
 
 const useAllChats = () => {
   const [chatSessions, setChatSessions] = useState<ChatSession[]>([]);
+  const [loading, setLoading] = useState(false);
+  const [isApiCallOngoing, setIsApiCallOngoing] = useState(false);
+
+  const deleteChat = async (sessionId: number) => {
+    setIsApiCallOngoing(true);
+    try {
+      await axiosAuthInstance.delete(`/messaging/chat-session/${sessionId}`);
+      setChatSessions((prevChatSessions) =>
+        prevChatSessions.filter(({ id }) => id !== sessionId)
+      );
+    } catch (error) {
+      console.error("Error deleting chat sessions:", error);
+    } finally {
+      setIsApiCallOngoing(false);
+    }
+  };
 
   useEffect(() => {
     const fetchChatSessions = async () => {
+      setLoading(true);
       try {
         const response = await axiosAuthInstance.get("/messaging/chat-session");
         setChatSessions(response.data);
       } catch (error) {
         console.error("Error fetching chat sessions:", error);
+      } finally {
+        setLoading(false);
       }
     };
 
     fetchChatSessions();
   }, []);
 
-  return { chatSessions };
+  return { chatSessions, deleteChat, isApiCallOngoing, loading };
 };
 
 export default useAllChats;
